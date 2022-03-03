c6516837a1ba56a2b3942ed58707d47e84dd9863
uche.rstudio.server <uchembaka@gmail.com>
2022-03-03 14:15:54 +0000

    POFD to list function removed

--- Functions/summary.POFD.R
+++ Functions/summary.POFD.R
@@ -1,4 +1,3 @@
-source("helper.func.R")
 
 summary.POFD <- function(POFD, plot.Freq = T){
   comp <- apply(!is.na(POFD$POFDs),1,all)
@@ -8,12 +7,4 @@ summary.POFD <- function(POFD, plot.Freq = T){
   p <- sapply(1:ncol(POFD$POFDs.True.Functions), function(i) sum(length(which(!is.na(POFD$POFDs.True.Functions[,i])))))
   barplot(p, xlab = "t", ylab = "Freq")
   print(summary(rowSums(is.na(POFD$POFDs[!comp,]))/length(POFD$Grid)))
-}
-
-POFD.sim2List <- function(FD.mat){
-  grid <- seq(0,1, len=ncol(FD.mat)) 
-  Ly <- split(t(FD.mat), rep(1:ncol(t(FD.mat)), each = nrow(t(FD.mat))))
-  Lt <- lapply(Ly, function(i) grid[!is.na(i)])
-  Ly <- lapply(Ly, function(i) i[!is.na(i)])
-  return(list(Ly, Lt))
 }
